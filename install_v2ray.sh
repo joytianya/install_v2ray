b5aefc2765b2f1c3d9a0b683a6d9869af2c466ed
root <root@176.122.146.200.16clouds.com>
2022-12-13 02:49:47 -0500

    update

--- install_v2ray.sh
+++ install_v2ray.sh
@@ -8,6 +8,7 @@ source <(curl -sL https://multi.netlify.app/v2ray.sh) --remove
 source <(curl -sL https://multi.netlify.app/v2ray.sh) --zh
 v2ray update v4.45.2
 v2ray update v4.45.2
+echo 'Environment="V2RAY_VMESS_AEAD_FORCED=false"'>> /etc/systemd/system/v2ray.service
 systemctl daemon-reload
 systemctl restart v2ray.service
 systemctl reset-failed v2ray.service
