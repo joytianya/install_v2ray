0f45a7ef15eb87660929ce104b2af900a907e3de
root <root@176.122.146.200.16clouds.com>
2022-12-13 03:02:10 -0500

    update

--- install_v2ray.sh
+++ install_v2ray.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
 #https://github.com/Jrohy/multi-v2ray
-#vi /etc/systemd/system/v2ray.service
-# Environment="V2RAY_VMESS_AEAD_FORCED=false"
-#ExecStart=/usr/bin/env v2ray.vmess.aead.forced=false /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json
+
 systemctl stop firewalld.service
 systemctl disable firewalld.service
 source <(curl -sL https://multi.netlify.app/v2ray.sh) --remove
 source <(curl -sL https://multi.netlify.app/v2ray.sh) --zh
 v2ray update v4.45.2
-v2ray update v4.45.2
-echo 'Environment="V2RAY_VMESS_AEAD_FORCED=false"'>> /etc/systemd/system/v2ray.service
 
+# 修改环境变量
+# https://github.com/233boy/v2ray/issues/812
+#vi /etc/systemd/system/v2ray.service
+# Environment="V2RAY_VMESS_AEAD_FORCED=false"
+# 或者ExecStart=/usr/bin/env v2ray.vmess.aead.forced=false /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json
 systemctl daemon-reload
 systemctl restart v2ray.service
 systemctl reset-failed v2ray.service
